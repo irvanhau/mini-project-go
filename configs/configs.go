5dc635456d731e4d330a80d8dc12a3be941b1881
Irvan Hauwerich <92709205+irvanhau@users.noreply.github.com>
2023-11-23 10:45:41 +0700

    Update configs.go

--- configs/configs.go
+++ configs/configs.go
@@ -39,12 +39,12 @@ func InitConfig() *ProgramConfig {
 func loadConfig() *ProgramConfig {
 	var res = new(ProgramConfig)
 
-	// err := godotenv.Load(".env")
+	err := godotenv.Load(".env")
 
-	// if err != nil {
-	// 	logrus.Error("Config : Cannot load config file, ", err.Error())
-	// 	return nil
-	// }
+	if err != nil {
+		logrus.Error("Config : Cannot load config file, ", err.Error())
+		return nil
+	}
 
 	if val, found := os.LookupEnv("SERVER"); found {
 		port, err := strconv.Atoi(val)
